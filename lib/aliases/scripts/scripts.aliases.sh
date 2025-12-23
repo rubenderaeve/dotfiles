00c113b5127692213249c442c4a2f5e2fc5a9823
Ruben De Raeve <rderaeve@gmail.com>
2025-12-23 10:41:40 +0100

    create script aliases

---
+++ lib/aliases/scripts/scripts.aliases.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+alias bt='bash /home/drube/tls/scripts/bt.sh'
+alias bcb='bash /home/drube/tls/scripts/bcbuildScript.sh'
+alias tst='bash /home/drube/tls/scripts/testRunScript.sh'
+alias br='bash /home/drube/tls/scripts/runBricscad.sh'
+alias tapp='bash /home/drube/tls/scripts/launch_bcadtestapp.sh'
